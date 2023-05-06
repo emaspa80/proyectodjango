4a4021f2ac82ef4a066409b7d7f5577ba760ef26
Delfor Fuente <delforfuente@gmail.con>
2023-05-05 22:29:05 -0300

    New update

--- tienda_vitual/app_t_virtual/forms.py
+++ tienda_vitual/app_t_virtual/forms.py
@@ -1,6 +1,27 @@
 from django import forms
 
 class ContactoForm(forms.Form):
-    nombre = forms.CharField(label="Nombre", required=True)
-    apellido = forms.CharField(label="Apellido", required=True)
-    email = forms.EmailField(label="Email", required=True)
+    nombre = forms.CharField(
+        label="Nombre", 
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+        required=True,
+        max_length=256,
+    )
+    apellido = forms.CharField(
+        label="Apellido",
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+        required=True,
+        max_length=256,
+    )
+    email = forms.EmailField(
+        label="Email", 
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+        required=True,
+        # max_length=256,        
+    )
+    mensaje =forms.CharField(
+        label='Mensaje',
+        widget=forms.Textarea(attrs={'rows': 5, 'class': 'form-control'}),
+        required=True,
+        max_length=256,
+    )
