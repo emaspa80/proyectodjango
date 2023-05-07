1e9f61359afa486ab17c1065d0d5e578b9bdbd4f
agdiazfermin <116310687+agdiazfermin@users.noreply.github.com>
2023-05-07 12:18:04 -0300

    Modelo propuesto

--- tienda_vitual/app_t_virtual/models.py
+++ tienda_vitual/app_t_virtual/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Productos(models.Model):
+    nombre=models.CharField(max_length=30)
+    producto=models.CharField(max_length=20) #Para definir remera, buzo o cualquier otro
+    precio=models.IntegerField()
+    oferta=models.BooleanField() #Para activar o no si esta en oferta
+    valid=models.BooleanField()  #Para activar o no el producto en la vista
+
+class Pedidos(models.Model):
+    numero=models.IntegerField()
+    fecha=models.DateField()
+    entregado=models.BooleanField()
+
+class Clientes(models.Model):
+    nombre=models.CharField(max_length=30)
+    direccion=models.CharField(max_length=50)
+    email=models.EmailField()
+    telefono=models.CharField(max_length=11)
