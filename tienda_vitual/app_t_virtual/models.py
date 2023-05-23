90dbc619d36e6e9a9f7de6c360828d9d5b20eff0
agdiazfermin <116310687+agdiazfermin@users.noreply.github.com>
2023-05-23 00:36:58 -0300

    nuevo modelo

--- tienda_vitual/app_t_virtual/models.py
+++ tienda_vitual/app_t_virtual/models.py
@@ -1,20 +1,23 @@
 from django.db import models
 
 # Create your models here.
-class Productos(models.Model):
-    nombre=models.CharField(max_length=30)
-    producto=models.CharField(max_length=20) #Para definir remera, buzo o cualquier otro
-    precio=models.IntegerField()
-    oferta=models.BooleanField() #Para activar o no si esta en oferta
-    valid=models.BooleanField()  #Para activar o no el producto en la vista
-
 class Pedidos(models.Model):
     numero=models.IntegerField()
     fecha=models.DateField()
     entregado=models.BooleanField()
+
+class Productos(models.Model):
+    nombre=models.CharField(max_length=30)
+    talle=models.CharField(max_length=4,null=True)
+    producto=models.CharField(max_length=20) #Para definir remera, buzo o cualquier otro
+    precio=models.IntegerField()
+    oferta=models.BooleanField() #Para activar o no si esta en oferta
+    valid=models.BooleanField()  #Para activar o no el producto en la vista
+    pedido = models.ManyToManyField(Pedidos) # Muchos a muchos pedidos y productos
 
 class Clientes(models.Model):
     nombre=models.CharField(max_length=30)
     direccion=models.CharField(max_length=50)
     email=models.EmailField()
     telefono=models.CharField(max_length=11)
+    pedidos = models.ForeignKey(Pedidos, on_delete=models.CASCADE, null=True) # muchos a uno pedidos a clientes
