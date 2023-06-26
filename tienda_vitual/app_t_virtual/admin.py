2a04c01efbc620c5872c6e9ade33d3c79971a37e
agdiazfermin <116310687+agdiazfermin@users.noreply.github.com>
2023-06-25 21:15:38 -0300

    Habilitando admin

--- tienda_vitual/app_t_virtual/admin.py
+++ tienda_vitual/app_t_virtual/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Pedidos, Productos, Clientes
 # Register your models here.
+admin.site.register(Pedidos)
+admin.site.register(Productos)
+admin.site.register(Clientes)
